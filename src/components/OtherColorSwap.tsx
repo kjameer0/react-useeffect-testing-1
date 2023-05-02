1e79dcfa6dbe0e6252df6640a41070886b93d953
Khalid Jameer <kjameer0@gmail.com>
2023-05-02 16:21:37 -0400

    chore: commented simple explanations

--- src/components/OtherColorSwap.tsx
+++ src/components/OtherColorSwap.tsx
@@ -2,15 +2,19 @@ import { useEffect, useState } from 'react';
 export default function OtherColorSwap() {
   const [count, setCount] = useState(0);
   const [color, setColor] = useState('red');
+  //use this width to see how even layout changes don't trigger parent rerenders
   const [width, setWidth] = useState('100px');
   //create a hook that changes the background color of a div every second
   useEffect(() => {
+    //adjusting state in an interval function
+    //and clearing that interval in cleanup
     const interval = setInterval(() => {
       setColor((prev) => (prev === 'red' ? 'blue' : 'red'));
       setCount((prev) => prev + 1);
       console.log('interval function');
       setWidth((prev) => (prev === '100px' ? '150px' : '100px'));
     }, 1000);
+    //cleanup function runs every render
     return () => {
       console.log('red-blue render');
       console.log('count red blue', count);
@@ -26,7 +30,7 @@ export default function OtherColorSwap() {
   }
   return (
     <div className="box-container">
-      <div className="test-box" style={{ backgroundColor: color, width: width }}>
+      <div className="test-box" style={{ backgroundColor: color }}>
         <button onClick={addCountClick}>Add to Count</button>
       </div>
       <p className="count-text">{count}</p>
